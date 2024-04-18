b441bdd1c89b9d7960fd0a0e616826978ee03a97
Diogo Venes <minigamerptpt@gmail.com>
2024-04-18 10:56:38 +0100

    Update websitepage.ts

--- frontend/src/app/websitepage.ts
+++ frontend/src/app/websitepage.ts
@@ -4,6 +4,6 @@ export interface WebsitePage {
   _id: string; // Optional if using MongoDB ObjectId
   website: Website; // Reference to the parent website's _id
   url: string;
-  lastEvalDate: Date;
+  lastEvalDate?: Date;
   pageState: string;
 }
