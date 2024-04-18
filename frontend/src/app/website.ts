35bb15dc410eef4801664343738c3faf11bc3f8a
Diogo Venes <minigamerptpt@gmail.com>
2024-04-18 10:28:01 +0100

    Added entities

---
+++ frontend/src/app/website.ts
@@ -0,0 +1,10 @@
+import {WebsitePage} from "./websitepage";
+
+export interface Website {
+  _id: string; // Optional if using MongoDB ObjectId
+  url: string;
+  registerDate: Date;
+  lastEvalDate: Date;
+  monitorState: string;
+  webpages: WebsitePage[];
+}
---
+++ frontend/src/app/websitepage.ts
@@ -0,0 +1,9 @@
+import {Website} from "./website";
+
+export interface WebsitePage {
+  _id: string; // Optional if using MongoDB ObjectId
+  website: Website; // Reference to the parent website's _id
+  url: string;
+  lastEvalDate: Date;
+  pageState: string;
+}
