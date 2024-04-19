81b7808fc876c4f112bf65267c4f3e15c999436b
Diogo Venes <minigamerptpt@gmail.com>
2024-04-19 12:54:40 +0100

    hilarious changes

--- frontend/src/app/websiteadd/websiteadd.component.ts
+++ frontend/src/app/websiteadd/websiteadd.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 //import { WebsiteService } from '../website.service';
 
 @Component({
   selector: 'app-websiteadd',
   templateUrl: './websiteadd.component.html',
   styleUrls: ['./websiteadd.component.css'],
+  animations: [
+    trigger('slideFade', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(100%)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ opacity: 0, transform: 'translateY(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class WebsiteAddComponent {
   websiteForm: FormGroup;
@@ -20,7 +32,6 @@ export class WebsiteAddComponent {
   }
 
   addWebsite(): void {
-    this.successMessage = '';
     if (this.websiteForm.valid) {
       const url = this.websiteForm.value.url.trim();
       // this.websiteService.addWebsite(url).subscribe(() => {
@@ -29,6 +40,10 @@ export class WebsiteAddComponent {
       console.log(url);
       this.websiteForm.reset();
       this.successMessage = 'Website added successfully';
+
+      setTimeout(() => {
+        this.successMessage = null;
+      } , 2000);
     }
   }
 }
